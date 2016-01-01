872bb68655996cab44bc7b8936a84511f4d80fa2
mike rumore <mike.a.rumore@gmail.com>
2016-01-01 14:27:26 -0600

    generic list functions.

--- rlib/generic_utils.R
+++ rlib/generic_utils.R
@@ -6,3 +6,22 @@ closedevs <- function()
     while (dev.cur() > 1) { dev.off() }
 }
 
+lnrow <- function(dfrm) 
+{
+    lapply(dfrm, nrow)
+}
+
+lncol <- function(dfrm) 
+{
+    lapply(dfrm, ncol)
+}
+
+lclass <- function(dfrm) 
+{
+    lapply(dfrm, class)
+}
+
+lhead <- function(dfrm) 
+{
+    lapply(dfrm, head)
+}
