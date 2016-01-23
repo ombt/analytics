1b349264782d3aa321ba1f0cb57e3b8cd1e74c81
mike.a.rumore <mike.a.rumore@gmail.com>
2016-01-22 20:39:02 -0600

    update

--- books/r_cookbook/ch14.R
+++ books/r_cookbook/ch14.R
@@ -1,3 +1,28 @@
+read_count_csv <- function(file="count.csv")
+{
+    read.table(file=file,
+               header=TRUE,
+               sep=";",
+               stringsAsFactors=FALSE)
+}
+
+sort_count_csv <- function(cnts)
+{
+    cnts[order(cnts$MACHINE, 
+               cnts$LANE, 
+               cnts$STAGE,
+               cnts$TIMESTAMP),]
+}
+
+unsorted.counts <- read_count_csv()
+
+attach(unsorted.counts)
+TErrors <- TPMiss+TRMiss+TDMiss+THMiss+TRSMiss+TMMiss
+TPlace  <- TPickup-TErrors
+detach()
+
+counts <- sort_count_csv(cbind(unsorted.counts,TPlace,TErrors))
+
 #
 # there are three R packages: ts, zoo and xts.
 # ts is rather limited. zoo and xts are better.
@@ -133,5 +158,148 @@ plot(z.cnts.ts,
 
 head(zoo.counts.ts)
 tail(zoo.counts.ts)
+
+
+#
+# read in CSV file
+#
+read_count_csv <- function(file="count.csv")
+{
+    read.table(file=file,
+               header=TRUE,
+               sep=";",
+               stringsAsFactors=FALSE)
+}
+
+#
+# > names(counts)
+# [1] "MACHINE"   "LANE"      "STAGE"     "PRODUCT"   "TIMESTAMP" "FID" 
+# [7] "FNAME"     "OUTPUT"    "SERIAL"    "PRODUCTID" "TPickup"   "TPMiss"
+# [13] "TRMiss"    "TDMiss"    "THMiss"    "TRSMiss"   "TMMiss"    "TPlace"
+#
+sort_count_csv <- function(cnts)
+{
+    cnts[order(cnts$MACHINE, 
+               cnts$LANE, 
+               cnts$STAGE,
+               cnts$TIMESTAMP),]
+}
+
+#
+# write sorted count data frame to a csv file
+#
+write_sorted_count_csv <- function(scnts, file="sorted.counts.csv")
+{
+    write.table(scnts, 
+                file=file, 
+                quote=FALSE, 
+                row.names=FALSE,
+                col.names=TRUE,
+                sep=";")
+}
+
+#
+# processing to create time series
+#
+cnts <- 
+    read_count_csv()
+
+attach(cnts)
+TErrors = TPMiss+TRMiss+TDMiss+THMiss+TRSMiss+TMMiss
+TPlace = TPickup-TErrors
+detach()
+
+sorted.cnts <- 
+    sort_count_csv(cbind(cnts,TPlace,TErrors))
+
+write_sorted_count_csv(sorted.cnts)
+
+grouped.sorted.cnts <- 
+    split(sorted.cnts, 
+          list(sorted.cnts$MACHINE,
+               sorted.cnts$LANE,
+               sorted.cnts$STAGE,
+               sorted.cnts$PRODUCT))
+
+nonzero.grouped.sorted.cnts <-
+    grouped.sorted.cnts[as.vector(lapply(grouped.sorted.cnts, 
+                                         nrow)) > 0]
+
+lapply(nonzero.grouped.sorted.cnts, nrow)
+
+nz.grp.scnts <- nonzero.grouped.sorted.cnts
+
+# x11()
+# par(mfrow=c(5,2))
+# 
+# for (i in 1:10)
+# {
+#     cntsi <- nz.grp.scnts[[i]]
+# 
+#     errsi <- cntsi$TErrors - min(cntsi$TErrors)
+# 
+#     ie <- seq_along(errsi)
+#     ip1e <- seq_along(errsi)+1
+# 
+#     ie <- ie[-length(ie)]
+#     ip1e <- ip1e[-length(ip1e)]
+# 
+#     derrsi = errsi[ip1e] - errsi[ie]
+#     derrsi
+#     sum(derrsi)
+# 
+#     plot(1:length(derrsi), derrsi)
+# }
+
+copy_only_flds <- c("MACHINE",
+                    "LANE",
+                    "STAGE",
+                    "PRODUCT",
+                    "TIMESTAMP",
+                    "FID",
+                    "FNAME",
+                    "OUTPUT",
+                    "SERIAL",
+                    "PRODUCTID")
+
+calculate_flds <- c("TPickup",
+                    "TPMiss",
+                    "TRMiss",
+                    "TDMiss",
+                    "THMiss",
+                    "TRSMiss",
+                    "TMMiss",
+                    "TPlace",
+                    "TErrors")
+
+fcnts <- nz.grp.scnts
+
+lapply(fcnts,nrow)
+
+for (name in names(fcnts))
+{
+    orig_nrow     <- nrow(fcnts[[name]])
+
+    assign_seq_no <- 1:(orig_nrow-1)
+    higher_seq_no <- 2:orig_nrow
+    lower_seq_no  <- 1:(orig_nrow-1)
+
+    for (fld in copy_only_flds)
+    {
+        fcnts[[name]][assign_seq_no,fld] <-
+            fcnts[[name]][higher_seq_no,fld]
+    }
+
+    for (fld in calculate_flds)
+    {
+        fcnts[[name]][assign_seq_no,fld] <-
+            fcnts[[name]][higher_seq_no,fld] -
+            fcnts[[name]][lower_seq_no,fld]
+    }
+
+    fcnts[[name]] <- fcnts[[name]][-orig_nrow,]
+}
+
+lapply(fcnts,nrow)
 
 
