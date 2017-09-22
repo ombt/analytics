275811c3260f01e7f114adc254eecd129f29c5d2
mike.a.rumore <mike.a.rumore@gmail.com>
2017-09-21 21:28:38 -0500

    apex bin

---
+++ apex/bin/pg.list.dbs.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+#
+use strict;
+#
+use DBI;
+#
+if (scalar(@ARGV) == 0)
+{
+    printf "usage: $0 host [user [passwd]]\n", ;
+    exit 0;
+}
+#
+my $host_name = undef;
+my $user_name = undef;
+my $password = undef;
+#
+die "missing host: $!" unless (scalar(@ARGV) >= 1);
+#
+$host_name = shift @ARGV;
+$user_name = shift @ARGV if (scalar(@ARGV) > 0);
+$password = shift @ARGV if (scalar(@ARGV) > 0);
+#
+#
+my $dsn = "dbi:Pg:dbname='';host=$host_name";
+#
+my $dbh = DBI->connect($dsn, $user_name, $password);
+#
+# my $sth = $dbh->prepare("select * from $table_name");
+# my $sth = $dbh->prepare("select count(*) from pg_tables where tablename = '$table_name' and schemaname = 'public'");
+my $sth = $dbh->prepare("select datname from pg_database");
+$sth->execute();
+while (my @data = $sth->fetchrow_array())
+{
+    printf "==>> %s\n", join("|", @data);
+}
+#
+$dbh->disconnect;
+#
+exit 0;
