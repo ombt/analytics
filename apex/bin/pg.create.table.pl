9be1e3848889b947d9542b4089f12d6fefd7204e
mike.a.rumore <mike.a.rumore@gmail.com>
2017-09-28 22:53:48 -0500

    apex bin

--- apex/bin/pg.create.table.pl
+++ apex/bin/pg.create.table.pl
@@ -6,24 +6,28 @@ use DBI;
 #
 if (scalar(@ARGV) == 0)
 {
-    printf "usage: $0 table host db [user [passwd]]\n", ;
+    printf "usage: $0 table fld1[:fld2...] host db [user [passwd]]\n", ;
     exit 0;
 }
 #
 my $table_name = undef;
+my $csv_fields = undef;
 my $host_name = undef;
 my $db_name = undef;
 my $user_name = undef;
 my $password = undef;
 #
-die "missing table, host, or db names: $!" unless (scalar(@ARGV) >= 3);
+die "missing table, host, or db names: $!" unless (scalar(@ARGV) >= 4);
 #
 $table_name = shift @ARGV;
+$csv_fields = shift @ARGV;
 $host_name = shift @ARGV;
 $db_name = shift @ARGV;
 #
 $user_name = shift @ARGV if (scalar(@ARGV) > 0);
 $password = shift @ARGV if (scalar(@ARGV) > 0);
+#
+my @fields = split /:/, $csv_fields;
 #
 my $dsn = "dbi:Pg:dbname=$db_name;host=$host_name";
 #
@@ -33,6 +37,7 @@ die "Unable to connect: $DBI::errstr\n" unless (defined($dbh));
 my $sth = $dbh->prepare("select tablename from pg_tables where tablename = '$table_name' and schemaname = 'public'");
 $sth->execute();
 #
+my $found = 0;
 while (my @data = $sth->fetchrow_array())
 {
     if ($data[0] eq $table_name)
@@ -51,42 +56,50 @@ else
 {
     printf "==>> Table %s: NOT EXISTS. Creating.\n", $table_name;
     #
-    my $sql = "create database $db_name";
-    if (defined($user_name))
+    my $sql = "create table $table_name ( ";
+    #
+    while (scalar(@fields) > 0)
     {
-        $sql .= " owner $user_name";
+        my $field = shift @fields;
+        #
+        $sql .= "$field text, ";
     }
     #
-    $sth = $dbh->prepare($sql);
-    if (defined($sth->execute()))
-    {
-        printf "Database created ... Checking again.\n";
-        $found = 0;
-        $sth = $dbh->prepare("select datname from pg_database");
-        $sth->execute();
-        while (my @data = $sth->fetchrow_array())
-        {
-            if ($data[0] eq $db_name)
-            {
-                $found = 1;
-	        last;
-            }
-        }
-        $sth = undef;
-        #
-        if ($found)
-        {
-            printf "==>> DB %s: EXISTS.\n", $db_name;
-        }
-        else
-        {
-            printf "==>> DB %s: NOT EXISTS. CREATE DATABASE FAILED.\n", $db_name;
-        }
-    }
-    else
-    {
-        printf "==>> DB %s: STILL DOES NOT EXISTS.\n", $db_name;
-    }
+    $sql =~ s/, *$//;
+    $sql .= " )";
+    #
+    printf "==>> SQL Insert command: %s\n", $sql;
+#     #
+#     $sth = $dbh->prepare($sql);
+#     if (defined($sth->execute()))
+#     {
+#         printf "Database created ... Checking again.\n";
+#         $found = 0;
+#         $sth = $dbh->prepare("select datname from pg_database");
+#         $sth->execute();
+#         while (my @data = $sth->fetchrow_array())
+#         {
+#             if ($data[0] eq $db_name)
+#             {
+#                 $found = 1;
+# 	        last;
+#             }
+#         }
+#         $sth = undef;
+#         #
+#         if ($found)
+#         {
+#             printf "==>> DB %s: EXISTS.\n", $db_name;
+#         }
+#         else
+#         {
+#             printf "==>> DB %s: NOT EXISTS. CREATE DATABASE FAILED.\n", $db_name;
+#         }
+#     }
+#     else
+#     {
+#         printf "==>> DB %s: STILL DOES NOT EXISTS.\n", $db_name;
+#     }
 }
 
 #
