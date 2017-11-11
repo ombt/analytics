406bef23431bfeb7ca0a61230f315d9ad9f934c1
mike.a.rumore <mike.a.rumore@gmail.com>
2017-11-11 15:04:58 -0600

    apex test bin

--- apex/testbin/mongo/mongo.list.collections.pl
+++ apex/testbin/mongo/mongo.list.collections.pl
@@ -9,23 +9,26 @@ use Safe::Isa;
 #
 if (scalar(@ARGV) == 0)
 {
-    printf "usage: $0 host [port [user [passwd]]]\n", ;
+    printf "usage: $0 db host [port [user [passwd]]]\n", ;
     exit 0;
 }
 #
+my $db_name = undef;
 my $host_name = undef;
 my $port = undef;
 my $user_name = undef;
 my $password = undef;
 #
-die "missing host: $!" unless (scalar(@ARGV) >= 1);
+die "missing host or db names: $!" unless (scalar(@ARGV) >= 2);
 #
-my $mongo_uri = undef;
-#
+$db_name = shift @ARGV;
 $host_name = shift @ARGV;
+#
 $port = shift @ARGV if (scalar(@ARGV) > 0);
 $user_name = shift @ARGV if (scalar(@ARGV) > 0);
 $password = shift @ARGV if (scalar(@ARGV) > 0);
+#
+my $mongo_uri = undef;
 #
 if (defined($port))
 {
@@ -55,8 +58,10 @@ my $client = undef;
 $client = MongoDB->connect($mongo_uri);
 $client->connect;
 #
-my @dbs = $client->database_names;
-printf "Host: %s MongoDBs:\n%s\n", $host_name, join("\n", @dbs);
+my $db = $client->get_database($db_name);
+my @collections = $db->collection_names;
+printf "Host: %s, DB: %s, Collections:\n%s\n", 
+       $host_name, $db_name, join("\n", @collections);
 #
 $client->disconnect;
 #
