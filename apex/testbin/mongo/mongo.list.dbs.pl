cac318eb6d61c4fe8392762d9485af77e1635e43
mike.a.rumore <mike.a.rumore@gmail.com>
2017-11-11 13:22:45 -0600

    apex bin

--- apex/testbin/mongo/mongo.list.dbs.pl
+++ apex/testbin/mongo/mongo.list.dbs.pl
@@ -2,7 +2,10 @@
 #
 use strict;
 #
-use DBI;
+use MongoDB;
+#
+use Try::Tiny;
+use Safe::Isa;
 #
 if (scalar(@ARGV) == 0)
 {
@@ -11,30 +14,50 @@ if (scalar(@ARGV) == 0)
 }
 #
 my $host_name = undef;
+my $port = undef;
 my $user_name = undef;
 my $password = undef;
 #
 die "missing host: $!" unless (scalar(@ARGV) >= 1);
 #
+my $mongo_uri = undef;
+#
 $host_name = shift @ARGV;
+$port = shift @ARGV if (scalar(@ARGV) > 0);
 $user_name = shift @ARGV if (scalar(@ARGV) > 0);
 $password = shift @ARGV if (scalar(@ARGV) > 0);
 #
-#
-my $dsn = "dbi:Pg:dbname='';host=$host_name";
-#
-my $dbh = DBI->connect($dsn, $user_name, $password, { PrintError => 0, RaiseError => 0 } );
-die "Unable to connect: $DBI::errstr\n" unless (defined($dbh));
-#
-# my $sth = $dbh->prepare("select * from $table_name");
-# my $sth = $dbh->prepare("select count(*) from pg_tables where tablename = '$table_name' and schemaname = 'public'");
-my $sth = $dbh->prepare("select datname from pg_database");
-$sth->execute();
-while (my @data = $sth->fetchrow_array())
+if (defined($port))
 {
-    printf "==>> %s\n", join("|", @data);
+    if (defined($user_name))
+    {
+        if (defined($password))
+        {
+            $mongo_uri = "mongodb://${user_name}:${password}@${host_name}:${port}";
+        }
+        else
+        {
+            $mongo_uri = "mongodb://${user_name}@${host_name}:${port}";
+        }
+    }
+    else
+    {
+        $mongo_uri = "mongodb://${host_name}:${port}";
+    }
+}
+else
+{
+    $mongo_uri = "mongodb://${host_name}";
 }
 #
-$dbh->disconnect;
+my $client = undef;
+#
+$client = MongoDB->connect($mongo_uri);
+$client->connect;
+#
+my @dbs = $client->database_names;
+printf "MongoDBs:\n%s\n", join("\n", @dbs);
+#
+$client->disconnect;
 #
 exit 0;
