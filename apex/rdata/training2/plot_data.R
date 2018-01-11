8ca469e90da612d4a93ed6bbd692178032daecd5
mike.a.rumore <mike.a.rumore@gmail.com>
2018-01-11 13:46:44 -0600

    apex bin - plot data to correlate with error results.

---
+++ apex/rdata/training2/plot_data.R
@@ -0,0 +1,408 @@
+ # install.packages("DBI")
+ # install.packages("RSQLite")
+ # warnings()
+ # install.packages("RSQLite")
+ # install.packages("RPostgreSQL")
+ # install.packages("ggplot2")
+ # install.packages("sqldf")
+ # install.packages("dplyr")
+ # install.packages("plyr")
+ # install.packages("plyr")
+ # library(plyr)
+ # q()
+ # ls()
+ # lf()
+ # source("source_all.R")
+ # lf()
+ # q()
+ # lf()
+ # q()
+ # lf()
+ # mysrc("pa_pca.R")
+ # mysrc("pa_pca.R")
+ # plot(paln.pr.pve, xlab="principal component", ylab="proportion of variance explained", ylim=c(0,1), type='b')
+ # closedevs()
+ # mysrc("pa_logit.R")
+ # mysrc("pa_logit.R")
+ # mysrc("pa_hclust.R")
+ # pa_hclust_for_cols()
+ # q()
+ # lf()
+ # lf
+ # glob2rx(pattern="*")
+ # glob2rx(pattern="*.*")
+ # glob2rx(pattern="*.apl")
+ # glob2rx(pattern="*.exe
+ # ")
+ # mysrc("pa_efa.R")
+ # install_and_load("psych")
+ # mysrc("pa_efa.R")
+ # mysrc("pa_efa.R")
+ # mysrc("pa_efa.R")
+ # mysrc("pa_efa.R")
+ # mysrc("pa_efa.R")
+ # mysrc("pa_efa.R")
+ # q()
+ # lf()
+ # xx=c()
+ # nrow(xx)
+ # xx=list()
+ # nrow(xx)
+ # xx=data.frame())
+ # xx=data.frame()
+ # nrow(xx)
+ # source("pa_hclust.R")
+ # rm(pa)
+ # lf("pq*")
+ # lf("pa*")
+ # pa_hclust_for_cols()
+ # source("pa_hclust.R")
+ # pa_hclust_for_cols(sink.file="hclust.out")
+ # source("pa_hclust.R")
+ # pa_hclust_for_cols(sink.file="hclust.out")
+ # pa_hclust_for_cols(sink.file="hclust.out",pa_hclust_cols=pa_count_hclust_cols)
+ # source("pa_hclust.R")
+ # pa_hclust_for_cols(sink.file="hclust.out",pa_hclust_cols=pa_count_hclust_cols)
+ # source("pa_hclust.R")
+ # pa_hclust_for_cols(sink.file="hclust.out",pa_hclust_cols=pa_count_hclust_cols)
+ # :w
+ # pa_hclust_for_cols(sink.file="hclust.out")
+ # q()
+ # q()
+ # source("load_data.R")
+ # mysrc("load_data.R")
+ # lf("*xxx*")
+ # pa_aoi = get_pcb_pa_aoi_status_per_machine("training_data2")
+ # pa_aoi = get_pcb_pa_aoi_status_per_machine("training_data2")
+ # nrows(pa_aoi)
+ # nrow(pa_aoi)
+ # names(pa_aoi)
+ # range(pa_aoi$_dpc_tpickup)
+ # range(pa_aoi$dpc_tpickup)
+ # range(pa_aoi$dpc_fwait)
+ # range(pa_aoi$dpc_board)
+ # options(max.print=999999)
+ # names(pa_aoi)
+ # range(pa_aoi$aoi_error_count)
+ # range(pa_aoi$dpt_idle)
+ # plot(pa_aoi$dpt_idle)
+ # abline(pa_aoi$dpt_idle)
+ # ?subset
+ # source("/tmp/zzz")
+ # source("/tmp/zzz")
+ # names(xxx)
+ # names(grouped_pa_aoi)
+ # source("/tmp/zzz")
+ # names(grouped_pa_aoi)
+ # ?split
+ # ?split
+ # source("/tmp/zzz")
+ # ?split
+ # names(grouped_pa_aoi)
+ # names(grouped_pa_aoi[["LINEA10;1;1;1;RSD160RA-SA-NEW;RSD160SA-05"]])
+ # q()
+ # lf("create*")
+ # lf("gro*")
+ # rm(grouped_pa_aoi)
+ # lf("gro*")
+ # create_grouped_pa_aoi()
+ # lf("gro*")
+ # q()
+ # lf("*aoi*")
+ # grp_pa_aoi=grouped_pa_aoi
+ # rm(grouped_pa_aoi)
+ # lf("*aoi*")
+ # name(grp_pa_aoi)
+ # names(grp_pa_aoi)
+ # plot(grp_pa_aoi[["LINEA10;1;1;1;RSD160NA;RSD160NA-06"]])
+ # plot(grp_pa_aoi[["LINEA10;1;1;1;RSD160NA;RSD160NA-06"]]$dpc_tpickup)
+ # names(grp_pa_aoi[["LINEA10;1;1;1;RSD160NA;RSD160NA-06"]])
+ # options(max.print=999999)
+ # names(grp_pa_aoi[["LINEA10;1;1;1;RSD160NA;RSD160NA-06"]])
+ # plot(grp_pa_aoi[["LINEA10;1;1;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # plot(grp_pa_aoi[["LINEA10;1;1;1;RSD160NA;RSD160NA-06"]]$aoi_error_count>0)
+ # plot(grp_pa_aoi[["LINEA10;1;1;1;RSD160NA;RSD160NA-06"]]$aoi_error_count==0)
+ # plot(grp_pa_aoi[["LINEA10;1;1;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # plot(grp_pa_aoi[["LINEA10;1;2;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # plot(grp_pa_aoi[["LINEA10;1;3;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # plot(grp_pa_aoi[["LINEA10;1;4;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # plot(grp_pa_aoi[["LINEA10;1;5;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # plot(grp_pa_aoi[["LINEA10;1;5;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # sum(grp_pa_aoi[["LINEA10;1;5;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # nrow(grp_pa_aoi[["LINEA10;1;5;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # length(grp_pa_aoi[["LINEA10;1;5;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # grp_pa_aoi[["LINEA10;1;5;1;RSD160NA;RSD160NA-06"]]
+ # grp_pa_aoi[["LINEA10;5;1;1;RSD160NA;RSD160NA-06"]]
+ # plot(grp_pa_aoi[["LINEA10;2;1;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # plot(grp_pa_aoi[["LINEA10;3;1;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # plot(grp_pa_aoi[["LINEA10;4;1;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # plot(grp_pa_aoi[["LINEA10;5;1;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # plot(grp_pa_aoi[["LINEA10;5;1;1;RSD160NA;RSD160NA-06"]]$)
+ # points(grp_pa_aoi[["LINEA10;5;1;1;RSD160NA;RSD160NA-06"]]$aoi_error_count)
+ # save.history()
+ # savehistory()
+ # 
+plot_data <- function(label="LINEA10;1;1;1;RSD160NA;RSD160NA-06")
+{
+    ymax = max(grp_pa_aoi[[label]]$dpc_tpickup)
+    ymin = -1
+    ylim=c(ymin,ymax)
+
+    plot(grp_pa_aoi[[label]]$dpc_tpickup, ylim=ylim, col="green")
+    points(grp_pa_aoi[[label]]$aoi_error_count, ylim=ylim, col="red")
+}
+
+plot_data2 <- function(label="LINEA10;1;1;1;RSD160NA;RSD160NA-06")
+{
+    ymax = max(grp_pa_aoi[[label]]$dpc_tpickup)
+    ymin = -1
+    ylim=c(ymin,ymax)
+
+    plot(grp_pa_aoi[[label]]$dpc_tpickup, ylim=ylim, col="green")
+    points(grp_pa_aoi[[label]]$aoi_error_count, ylim=ylim, col="red")
+
+    for (irow in 1:length(grp_pa_aoi[[label]]$aoi_error_count))
+    {
+        error_count = grp_pa_aoi[[label]]$aoi_error_count[irow]
+        if (error_count > 0)
+        {
+            abline(v=irow,col="red")
+        }
+    }
+}
+
+plot_data3 <- function(label="LINEA10;1;1;1;RSD160NA;RSD160NA-06")
+{
+    tpickup = grp_pa_aoi[[label]]$dpc_tpickup + 1
+    aoi_err = grp_pa_aoi[[label]]$aoi_error_count + 1
+
+    ymin = 1
+    ymax = max(tpickup)
+    ylim=c(ymin,ymax)
+
+    plot(tpickup, ylim=ylim, col="green", log="y")
+    points(aoi_err, ylim=ylim, col="red")
+
+    for (irow in 1:length(aoi_err))
+    {
+        error_count = aoi_err[irow]
+        if (error_count > 1)
+        {
+            abline(v=irow,col="red")
+        }
+    }
+}
+
+plot_data4 <- function(label="LINEA10;1;1;1;RSD160NA;RSD160NA-06",
+                       ycol="dpc_tpickup",
+                       errcol="aoi_error_count",
+                       plot_err=FALSE)
+{
+    ydata   = grp_pa_aoi[[label]][[ycol]]
+    errdata = grp_pa_aoi[[label]][[errcol]]
+
+    ymin = 0
+    ymax = max(ydata)
+    ylim=c(ymin,ymax)
+
+    plot(ydata, ylim=ylim, main=ycol, col="black")
+    if (plot_err == TRUE)
+    {
+        points(errdata, ylim=ylim, col="red")
+    }
+
+    for (irow in 1:length(errdata))
+    {
+        error_count = errdata[irow]
+        if (error_count > 0)
+        {
+            abline(v=irow,col="red")
+        }
+    }
+}
+
+ # ftf_filename_route
+ # ufd_machine_order
+ # ufd_lane_no"
+ # ufd_stage_no
+ # ftf_filename_timestamp
+ # ftf_filename"
+ # ftf_filename_type
+ # ftf_filename_id
+ # ufd_filename_id"
+ # ufd_date
+ # ufd_pcb_serial
+ # ufd_pcb_id"
+ # ufd_output_no
+ # ufd_pcb_id_lot_no
+ # ufd_pcb_id_serial_no"
+ # upx_filename_id
+ # upx_author
+ # upx_authortype"
+ # upx_comment
+ # upx_date
+ # upx_diff"
+ # upx_format
+ # upx_machine
+ # upx_mjsid"
+ # upx_version
+ # upi_filename_id
+ # upi_bcrstatus"
+ # upi_code
+ # upi_lane
+ # upi_lotname"
+ # upi_lotnumber
+ # upi_output
+ # upi_planid"
+ # upi_productid
+ # upi_rev
+ # upi_serial"
+ # upi_serialstatus
+ # upi_stage
+ # i_filename_id"
+ # i_cid
+ # i_timestamp
+ # i_crc"
+ # i_c2d
+ # i_recipename
+ # i_mid"
+ # p_filename_id
+ # p_p
+ # p_cmp"
+ # aoi_status
+ # p_sc
+ # p_pid"
+ # p_fc
+ # dpc_filename_id
+ # dpc_pcb_id"
+ # dpc_pcb_serial
+ # dpc_machine_order
+ # dpc_lane_no"
+ # dpc_stage_no
+ # dpc_timestamp
+ # dpc_mjsid"
+ # dpc_lotname
+ # dpc_output_no
+ # dpc_bndrcgstop"
+ # dpc_bndstop
+ # dpc_board
+ # dpc_brcgstop"
+ # dpc_bwait
+ # dpc_cderr
+ # dpc_cmerr"
+ # dpc_cnvstop
+ # dpc_cperr
+ # dpc_crerr"
+ # dpc_cterr
+ # dpc_cwait
+ # dpc_fbstop"
+ # dpc_fwait
+ # dpc_jointpasswait
+ # dpc_judgestop"
+ # dpc_lotboard
+ # dpc_lotmodule
+ # dpc_mcfwait"
+ # dpc_mcrwait
+ # dpc_mhrcgstop
+ # dpc_module"
+ # dpc_otherlstop
+ # dpc_othrstop
+ # dpc_pwait"
+ # dpc_rwait
+ # dpc_scestop
+ # dpc_scstop"
+ # dpc_swait
+ # dpc_tdispense
+ # dpc_tdmiss"
+ # dpc_thmiss
+ # dpc_tmmiss
+ # dpc_tmount"
+ # dpc_tpickup
+ # dpc_tpmiss
+ # dpc_tpriming"
+ # dpc_trbl
+ # dpc_trmiss
+ # dpc_trserr"
+ # dpc_trsmiss
+ # dpt_filename_id
+ # dpt_pcb_id"
+ # dpt_pcb_serial
+ # dpt_machine_order
+ # dpt_lane_no"
+ # dpt_stage_no
+ # dpt_timestamp
+ # dpt_mjsid"
+ # dpt_lotname
+ # dpt_output_no
+ # dpt_actual"
+ # dpt_bndrcgstop
+ # dpt_bndstop
+ # dpt_brcg"
+ # dpt_brcgstop
+ # dpt_bwait
+ # dpt_cderr"
+ # dpt_change
+ # dpt_cmerr
+ # dpt_cnvstop"
+ # dpt_cperr
+ # dpt_crerr
+ # dpt_cterr"
+ # dpt_cwait
+ # dpt_dataedit
+ # dpt_fbstop"
+ # dpt_fwait
+ # dpt_idle
+ # dpt_jointpasswait"
+ # dpt_judgestop
+ # dpt_load
+ # dpt_mcfwait"
+ # dpt_mcrwait
+ # dpt_mente
+ # dpt_mhrcgstop"
+ # dpt_mount
+ # dpt_otherlstop
+ # dpt_othrstop"
+ # dpt_poweron
+ # dpt_prdstop
+ # dpt_prod"
+ # dpt_prodview
+ # dpt_pwait
+ # dpt_rwait"
+ # dpt_scestop
+ # dpt_scstop
+ # dpt_swait"
+ # dpt_totalstop
+ # dpt_trbl
+ # dpt_trserr"
+ # dpt_unitadjust
+ # aoi_error_count"
+
+plot_data5 <- function(label="LINEA10;1;1;1;RSD160NA;RSD160NA-06",
+                       errcol="aoi_error_count",
+                       plot_err=FALSE)
+{
+    for (ycol in names(grp_pa_aoi[[label]]))
+    {
+        rycol=range(grp_pa_aoi[[label]][[ycol]])
+        if (is.numeric(rycol[2]) && (rycol[2] > 0))
+        {
+            answer = readline(prompt=sprintf("Plot %s [y/n/cr=y]: ", ycol))
+            if ((answer == "y") ||
+                (answer == ""))
+            {
+                plot_data4(label,
+                           ycol=ycol,
+                           errcol=errcol,
+                           plot_err=plot_err)
+            }
+            else if (answer == "q")
+            {
+                stop("Quitting ...")
+            }
+        }
+        else
+        {
+            print(sprintf("Skipping column %s ...", ycol))
+        }
+    }
+}
